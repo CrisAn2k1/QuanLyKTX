b18999398bf6ab633b2da72c11acf4747fc969f1
Your Name <you@example.com>
2022-03-30 00:32:33 +0700

    register

--- src/main/java/com/duongan/QuanLyKTX/config/SecurityConfig.java
+++ src/main/java/com/duongan/QuanLyKTX/config/SecurityConfig.java
@@ -31,13 +31,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http
             .authorizeRequests()
-                .antMatchers("/","/assets/**").permitAll()
+                .antMatchers("/register","/process_register","/assets/**").permitAll()
                 .anyRequest().authenticated().
             and()
                 .formLogin().loginPage("/login").permitAll()
                 .defaultSuccessUrl("/login?success=true")
                 .failureUrl("/login?success=fail")
                 .loginProcessingUrl("/j_spring_security_check").
+
             and().logout(logout -> logout
                 .logoutUrl("/logout")
                         .logoutSuccessUrl("/homepage")
--- src/main/java/com/duongan/QuanLyKTX/repository/UserRepository.java
+++ src/main/java/com/duongan/QuanLyKTX/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.duongan.QuanLyKTX.repository;
+import com.duongan.QuanLyKTX.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.Optional;
-public class UserRepository  {
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    public User findByEmail(String email);
 }
